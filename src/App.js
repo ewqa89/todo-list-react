89073707ab36dc2257bde13ba69d1289923a8717
Ewa Paszkowska <ewqa89@gmail.com>
2022-11-29 22:23:01 +0100

    formated text

--- src/App.js
+++ src/App.js
@@ -6,25 +6,23 @@ import Tasks from "./Tasks";
 import Container from "./Container";
 
 const tasks = [
+    { id: 1, content: "przejść na react", done: true },
+    { id: 2, content: "zjeść obiad", done: false },
 ];
 const hideDoneTasks = false;
 
 function App() {
-	
     return (
         <Container>
-        <Header title="Lista zadań" 
-        />
-        <Section 
-        title="Dodaj nowe zadanie" 
-        body={<Form />} 
-        />
-        
-        <Section
-        title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraHeaderContent={ <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        />
+            <Header title="Lista zadań" />
+            <Section
+                title="Dodaj nowe zadanie"
+                body={<Form />} />
+
+            <Section
+                title="Lista zadań"
+                body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+                extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
         </Container>
     );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -3,8 +3,7 @@ import "./style.css";
 const Form = () => (
     <form className="form">
         <fieldset className="form__fildset">
-        <input className="form__input" placeholder="Co jest do zrobienia?" autofocus 
-        />
+        <input className="form__input" placeholder="Co jest do zrobienia?" autofocus />
         <button type="submit" className="form__button">Dodaj zadanie</button>
         </fieldset>
     </form>
