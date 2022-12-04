488c398db35a10221c997dcbaa23dad24a0a2b77
Ewa Paszkowska <ewqa89@gmail.com>
2022-12-04 14:22:14 +0100

    rename div to header

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,10 +2,10 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
-        <div className="section__header">
+        <header className="section__header">
             <h2 className="section__head">{title}</h2>
             {extraHeaderContent}
-        </div>
+        </header>
         {body}
     </section>
 );
