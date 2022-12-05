b45a2e57eee9559f5c62743b61020736f0776660
Ewa Paszkowska <ewqa89@gmail.com>
2022-12-05 14:31:37 +0100

    st all done interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
   tasks.length > 0 && (
     <section className="buttons">
       <button className="buttons__button" onClick={toggleHideDone}>
         {hideDone ? "Pokaż ukończone" : "Ukryj ukończone"}
       </button>
-      <button className="buttons__button"
+      <button
+        className="buttons__button"
+        onClick={setAllDone}
         disabled={tasks.every(({ done }) => done)}
       >
         Ukończ wyszystkie
